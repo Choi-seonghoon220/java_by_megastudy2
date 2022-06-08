48f431f7a7dfb4f675d0fe45c0d82a3916c2ffa1
ilbtJ <ilbtjesus220@gmail.com>
2022-06-08 15:51:14 +0900

    Complete test11

--- java12/src/test11/Ex1.java
+++ java12/src/test11/Ex1.java
@@ -1,5 +1,8 @@
 package test11;
 
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+
 public class Ex1 {
 	
 	public static void main(String[] args) {
@@ -16,5 +19,36 @@ public class Ex1 {
 		System.out.println("이름순으로 정렬....");
 		sort(arr, (a, b) -> a.name.compareTo(b.name) < 0);
 		view(arr, System.out::println);
+	}
+	static void sort(Data[] arr, BiPredicate<Data, Data> func) {
+		Data temp;
+		
+		for (int i = 1; i < arr.length; i++) {
+			temp = arr[i];
+			
+			int j = i - 1;
+			
+			while (j >= 0 && func.test(temp, arr[j])) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = temp;
+		}
+	}
+	static void view(Data[] arr, Consumer func) {
+		for (Data v : arr) {
+			func.accept(v);
+		}
+	}
+}
+class Data {
+	String name;
+	int age;
+	Data(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String toString() {
+		return this.name + " " + this.age;
 	}
 }
